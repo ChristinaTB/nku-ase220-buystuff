54bf5e1a0f95c836459dc2a1f0834063e37d9771
Christina <burchfieldchristina@gmail.com>
2021-05-07 12:35:04 -0400

    update database

--- API/lib_db.php
+++ API/lib_db.php
@@ -1,27 +1,9 @@
 <?php
-set the parameters for the database
-$host = 'localhost:3308';
-$db= 'buystuff';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
-$dsn= "mysql:host=$host;dbname=$db;charset=$charset";
-
-$opt = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-
-//Creates a new connection
-$pdo= new PDO($dsn, $user, $pass, $opt);
-
-
-
-// $host = 'us-cdbr-east-03.cleardb.com';
-// $db= 'heroku_a89114b2c9653f0';
-// $user = 'b86f3046bbfa63';
-// $pass = '6b3e5433';
+// set the parameters for the database
+// $host = 'localhost:3308';
+// $db= 'buystuff';
+// $user = 'root';
+// $pass = '';
 // $charset = 'utf8';
 // $dsn= "mysql:host=$host;dbname=$db;charset=$charset";
 
@@ -33,6 +15,24 @@ $pdo= new PDO($dsn, $user, $pass, $opt);
 
 // //Creates a new connection
 // $pdo= new PDO($dsn, $user, $pass, $opt);
+
+
+
+$host = 'us-cdbr-east-03.cleardb.com';
+$db= 'heroku_a89114b2c9653f0';
+$user = 'b86f3046bbfa63';
+$pass = '6b3e5433';
+$charset = 'utf8';
+$dsn= "mysql:host=$host;dbname=$db;charset=$charset";
+
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+//Creates a new connection
+$pdo= new PDO($dsn, $user, $pass, $opt);
 
 
 
