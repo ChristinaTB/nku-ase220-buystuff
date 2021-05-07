66b3cdaf0205137d55f81095936fd2437ffe56a0
Christina <burchfieldchristina@gmail.com>
2021-05-07 12:00:25 -0400

    query

--- API/index.php
+++ API/index.php
@@ -13,13 +13,13 @@ $stmt->execute([]);
 //to seperate html and php and put into json
 die( json_encode($stmt->fetchAll()));
 
-$pdo ->query('CREATE TABLE `items` (
-    `ID` int(11) NOT NULL,
-    `item_name` varchar(96) CHARACTER SET utf16 NOT NULL,
-    `item_price` int(11) NOT NULL,
-    `item_detail` varchar(144) NOT NULL,
-    `item_picture` varchar(150) NOT NULL,
-    `user_ID` int(11) NOT NULL
+$pdo ->query('CREATE TABLE items (
+    ID int(11) NOT NULL,
+    item_name varchar(96) CHARACTER SET utf16 NOT NULL,
+    item_price int(11) NOT NULL,
+    item_detail varchar(144) NOT NULL,
+    item_picture varchar(150) NOT NULL,
+    user_ID int(11) NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8')
   
 
