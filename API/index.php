0557674040f692e89106e80d3a42b3aa2a85f4b0
Christina <burchfieldchristina@gmail.com>
2021-05-07 12:27:10 -0400

    update

--- API/index.php
+++ API/index.php
@@ -3,7 +3,7 @@ require(__DIR__.'/lib_db.php');
 require(__DIR__.'/lib_session.php');
 
 
-$stmt = $pdo->query('CREATE TABLE items (
+$stmt2 = $pdo->query('CREATE TABLE items (
     ID int(11) NOT NULL,
     item_name varchar(96) CHARACTER SET utf16 NOT NULL,
     item_price int(11) NOT NULL,
@@ -12,14 +12,24 @@ $stmt = $pdo->query('CREATE TABLE items (
     user_ID int(11) NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8)');
 
-
+$stmt2->execute([]);
 
 $stmt = $pdo->prepare('SELECT * FROM items');
 $stmt->execute([]);
 
 //telling the browser its json and not an html document
-header('Content-type:application/json');
+// header('Content-type:application/json');
 
 //to seperate html and php and put into json
 die( json_encode($stmt->fetchAll()));
 
+?>
+
+
+<html>
+    <head>
+</head>
+<body>
+    here
+</body>
+</html>
