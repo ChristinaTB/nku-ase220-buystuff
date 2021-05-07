f9ea0bca9445ed468928af5c60c4892962e06c3d
Christina <burchfieldchristina@gmail.com>
2021-05-07 11:15:03 -0400

    heroku database query

--- API/index.php
+++ API/index.php
@@ -13,9 +13,16 @@ header('Content-type:application/json');
 //to seperate html and php and put into json
 die( json_encode($stmt->fetchAll()));
 
-
-
-
+$pdo ->query(
+"CREATE TABLE items (
+    ID int(11) NOT NULL,
+    item_name varchar(96) CHARACTER SET utf16 NOT NULL,
+    item_price int(11) NOT NULL,
+    item_detail varchar(144) NOT NULL,
+    item_picture varchar(150) NOT NULL,
+    user_ID int(11) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+  );
 
 
 ?>
